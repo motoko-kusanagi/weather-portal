5df72c658ece752363769f6f0f5892a18c07e511
root <root@pinkpanther.psbiuro.pl>
2014-02-19 19:43:21 +0000

    added new options...

--- get_data.rb
+++ get_data.rb
@@ -12,18 +12,17 @@ xml = Nokogiri::XML(open("http://www.test.tatrynet.pl/pogoda/weatherMiddleware_v
 xml_date = xml.xpath('//dateTimeStr').map { |node| node.text }.first
 xml_place = xml.xpath('//nazwa').map { |node| node.text }
 xml_temperature = xml.xpath('//temperatura/aktualna').map { |node| node.text }
-xml_wind = xml.xpath('//wiatr/silaAvg').map { |node| node.text }
+xml_windAVG = xml.xpath('//wiatr/silaAvg').map { |node| node.text }
+xml_windMAX = xml.xpath('//wiatr/silaMax').map { |node| node.text }
+xml_windDIR = xml.xpath('//wiatr/kierunek').map { |node| node.text }
 
 for i in 0..xml_place.length-1
-  valley[i] = [weather.new(xml_place[i],xml_temperature[i],xml_wind[i],xml_date)]
+  valley[i] = [weather.new(xml_place[i],xml_temperature[i],xml_windAVG[i],xml_date)]
 end
 
 def xml_parse(what)
   return what.gsub('"',"").gsub("[","").gsub("]","")
 end
-
-xml_parse(xml_temperature.to_s)
-puts "#{xml_parse(xml_temperature.to_s)}, #{xml_date.to_s}"
 
 begin
   db = Mysql.new('localhost', 'ruby', 'github', 'topr')
@@ -33,3 +32,6 @@ rescue Mysql::Error
 end
 
 db.query("INSERT INTO temperature (GORYCZKOWA, PIEC_STAWOW, MORSKIE_OKO, DATE_XML, DATE_SYSTEM) VALUES (#{xml_parse(xml_temperature.to_s)}, '#{xml_date.to_s}', '#{Time.now.utc.to_s.gsub(" UTC","")}');")
+db.query("INSERT INTO wind_speed_averange (GORYCZKOWA, PIEC_STAWOW, MORSKIE_OKO, DATE_XML, DATE_SYSTEM) VALUES (#{xml_parse(xml_windAVG.to_s)}, '#{xml_date.to_s}', '#{Time.now.utc.to_s.gsub(" UTC","")}');")
+db.query("INSERT INTO wind_speed_maximum (GORYCZKOWA, PIEC_STAWOW, MORSKIE_OKO, DATE_XML, DATE_SYSTEM) VALUES (#{xml_parse(xml_windMAX.to_s)}, '#{xml_date.to_s}', '#{Time.now.utc.to_s.gsub(" UTC","")}');")
+db.query("INSERT INTO wind_direction (GORYCZKOWA, PIEC_STAWOW, MORSKIE_OKO, DATE_XML, DATE_SYSTEM) VALUES (#{xml_parse(xml_windDIR.to_s)}, '#{xml_date.to_s}', '#{Time.now.utc.to_s.gsub(" UTC","")}');")
