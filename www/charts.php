95ab2a33c07a104e6b8f750115f94cb346b6a160
Your Name <you@example.com>
2014-02-20 13:53:07 +0000

    added new chars

--- www/charts.php
+++ www/charts.php
@@ -4,13 +4,17 @@
   mysql_select_db('topr', $con);
   // write your SQL query here (you may use parameters from $_GET or $_POST if you need them)
   $query = mysql_query('SELECT * FROM temperature');
+  $query_windAVG = mysql_query('SELECT * FROM wind_speed_averange');
+  $query_windMAX = mysql_query('SELECT * FROM wind_speed_maximum');
+
+//*** TEMPERATURE *** //
 
   $table = array();
   $table['cols'] = array(
-    array('label' => 'DATE', 'type' => 'string'),
-    array('label' => 'GORYCZKOWA', 'type' => 'number'),
-    array('label' => 'MORSKIE OKO', 'type' => 'number'),
-    array('label' => 'PIEC STAWOW', 'type' => 'number')
+    array('label' => 'Date', 'type' => 'string'),
+    array('label' => 'Goryczkowa', 'type' => 'number'),
+    array('label' => 'Morskie Oko', 'type' => 'number'),
+    array('label' => 'Piec Stawow', 'type' => 'number')
   );
 
   $rows = array();
@@ -26,9 +30,63 @@
 
   $table['rows'] = $rows;
 
-$json = json_encode($table);
+$jsonTEMP = json_encode($table);
+
+
+// *** WIND AVG *** //
+
+  $table = array();
+  $table['cols'] = array(
+    array('label' => 'Date', 'type' => 'string'),
+    array('label' => 'Goryczkowa', 'type' => 'number'),
+    array('label' => 'Morskie Oko', 'type' => 'number'),
+    array('label' => 'Piec Stawow', 'type' => 'number')
+  );
+
+  $rows = array();
+  while($r = mysql_fetch_assoc($query_windAVG)) {
+    $temp = array();
+    $temp[] = array('v' => $r['DATE_XML']);
+    $temp[] = array('v' => $r['GORYCZKOWA']);
+    $temp[] = array('v' => $r['MORSKIE_OKO']);
+    $temp[] = array('v' => $r['PIEC_STAWOW']);
+
+    $rows[] = array('c' => $temp);
+  }
+
+  $table['rows'] = $rows;
+
+$json_windAVG = json_encode($table);
+
+
+// *** WIND MAX *** //
+
+  $table = array();
+  $table['cols'] = array(
+    array('label' => 'Date', 'type' => 'string'),
+    array('label' => 'Goryczkowa', 'type' => 'number'),
+    array('label' => 'Morskie Oko', 'type' => 'number'),
+    array('label' => 'Piec Stawow', 'type' => 'number')
+  );
+
+  $rows = array();
+  while($r = mysql_fetch_assoc($query_windMAX)) {
+    $temp = array();
+    $temp[] = array('v' => $r['DATE_XML']);
+    $temp[] = array('v' => $r['GORYCZKOWA']);
+    $temp[] = array('v' => $r['MORSKIE_OKO']);
+    $temp[] = array('v' => $r['PIEC_STAWOW']);
+
+    $rows[] = array('c' => $temp);
+  }
+
+  $table['rows'] = $rows;
+
+$json_windMAX = json_encode($table);
 
 ?>
+
+//*** HTML BODY ***//
 
 <html>
     <head>
@@ -41,26 +99,65 @@ $json = json_encode($table);
     google.load('visualization', '1', {'packages':['corechart']});
 
     // Set a callback to run when the Google Visualization API is loaded.
-    google.setOnLoadCallback(drawChart);
+    google.setOnLoadCallback(drawChartTemp);
+    google.setOnLoadCallback(drawChartWindAvg);
+    google.setOnLoadCallback(drawChartWindMax);
 
-    function drawChart() {
+    function drawChartTemp() {
 
       // Create our data table out of JSON data loaded from server.
-      var data = new google.visualization.DataTable(<?=$json?>);
+      var data = new google.visualization.DataTable(<?=$jsonTEMP?>);
           var options = {
-          title: 'TEMPERATURA',
+          title: 'TEMPERATURA [*C]',
           is3D: 'true',
           width: 1024,
           height: 400
         };
       // Instantiate and draw our chart, passing in some options.
       // Do not forget to check your div ID
-      var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
+      var chart = new google.visualization.LineChart(document.getElementById('chart_div_temp'));
       chart.draw(data, options);
     }
+
+	// DWA
+
+    function drawChartWindAvg() {
+
+      // Create our data table out of JSON data loaded from server.
+      var data = new google.visualization.DataTable(<?=$json_windAVG?>);
+          var options = {
+          title: 'SREDNIA PREDKOSC WIATRU [m/s]',
+          is3D: 'true',
+          width: 1024,
+          height: 400
+        };
+      // Instantiate and draw our chart, passing in some options.
+      // Do not forget to check your div ID
+      var chart = new google.visualization.LineChart(document.getElementById('chart_div_wind_avg'));
+      chart.draw(data, options);
+    }
+
+    function drawChartWindMax() {
+
+      // Create our data table out of JSON data loaded from server.
+      var data = new google.visualization.DataTable(<?=$json_windMAX?>);
+          var options = {
+          title: 'MAKSYMALNA PREDKOSC WIATRU [m/s]',
+          is3D: 'true',
+          width: 1024,
+          height: 400
+        };
+      // Instantiate and draw our chart, passing in some options.
+      // Do not forget to check your div ID
+      var chart = new google.visualization.LineChart(document.getElementById('chart_div_wind_max'));
+      chart.draw(data, options);
+    }
+
     </script>
   </head>
   <body>
-    <div id="chart_div"></div>
+    <div id="chart_div_temp"></div>
+    <div id="chart_div_wind_avg"></div>
+    <div id="chart_div_wind_max"></div>
   </body>
 </html>
