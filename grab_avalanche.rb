34c2349a3e0d2832f550b9f9f14166cf174ce442
Your Name <you@example.com>
2014-02-20 20:29:54 +0000

    added avalanche level + tend

--- grab_avalanche.rb
+++ grab_avalanche.rb
@@ -4,9 +4,22 @@ require 'open-uri'
 require 'nokogiri'
 require 'mysql'
 
-av_level = Nokogiri::HTML(open("http://www.topr.pl/index.php?option=com_content&task=view&id=130&Itemid=119"),'utf-8')
-av_level = av_level.css("div.imgstopien")
-puts av_level
+url = Nokogiri::HTML(open("http://www.topr.pl/index.php?option=com_content&task=view&id=130&Itemid=119"),'ISO-8859-2')
 
-#av_level = Nokogiri::HTML(open("http://www.topr.pl/index.php?option=com_content&task=view&id=130&Itemid=119"),'utf-8')
-#puts av_level.xpath('//img/alt')
+av_level = url.css('div.imgstopien img').map { |link| link['alt'] }
+av_level = av_level.to_s
+
+av_tend = url.css('div.tendency img').map { |link| link['alt'] }
+av_tend = av_tend.to_s.split(" ")
+av_tend = av_tend[1].to_s.gsub('"',"").gsub("]","").gsub(".","")
+
+if "#{av_tend}" == "rosn±ca" 
+  puts "#{av_level.chars[2].to_i}+"
+elsif "#{av_tend}" == "malej±ca"
+  puts "#{av_level.chars[2].to_i}-"
+elsif "#{av_tend}" == "stala"
+  puts "#{av_level.chars[2].to_i}"
+else
+  puts "NULL"
+end
+
