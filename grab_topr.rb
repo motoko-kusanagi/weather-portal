cb87faed171d75858fa5be90644cafb8e77fabab
Your Name <you@example.com>
2014-02-20 11:31:01 +0000

    ...

--- grab_topr.rb
+++ grab_topr.rb
@@ -4,8 +4,6 @@ require 'open-uri'
 require 'nokogiri'
 require 'mysql'
 
-datetime = Time.now.utc.to_s.gsub(" UTC","")
-
 xml = Nokogiri::XML(open("http://www.test.tatrynet.pl/pogoda/weatherMiddleware_v1.0/xml/lokalizacje1.xml"),'utf-8')
 xml_date = xml.xpath('//dateTimeStr').map { |node| node.text }.first
 xml_place = xml.xpath('//nazwa').map { |node| node.text }
@@ -13,6 +11,9 @@ xml_temperature = xml.xpath('//temperatura/aktualna').map { |node| node.text }
 xml_windAVG = xml.xpath('//wiatr/silaAvg').map { |node| node.text }
 xml_windMAX = xml.xpath('//wiatr/silaMax').map { |node| node.text }
 xml_windDIR = xml.xpath('//wiatr/kierunek').map { |node| node.text }
+
+datetime = Time.now.utc.to_s.gsub(" UTC","")
+xml_date = xml_date.to_s
 
 def xml_parse(what)
   return what.gsub('"',"").gsub("[","").gsub("]","")
@@ -25,7 +26,7 @@ rescue Mysql::Error
   exit 1
 end
 
-db.query("INSERT INTO temperature (GORYCZKOWA, PIEC_STAWOW, MORSKIE_OKO, DATE_XML, DATE_SYSTEM) VALUES (#{xml_parse(xml_temperature.to_s)}, '#{xml_date.to_s}', '#{datetime}');")
-db.query("INSERT INTO wind_speed_averange (GORYCZKOWA, PIEC_STAWOW, MORSKIE_OKO, DATE_XML, DATE_SYSTEM) VALUES (#{xml_parse(xml_windAVG.to_s)}, '#{xml_date.to_s}', '#{datetime}');")
-db.query("INSERT INTO wind_speed_maximum (GORYCZKOWA, PIEC_STAWOW, MORSKIE_OKO, DATE_XML, DATE_SYSTEM) VALUES (#{xml_parse(xml_windMAX.to_s)}, '#{xml_date.to_s}', '#{datetime}');")
-db.query("INSERT INTO wind_direction (GORYCZKOWA, PIEC_STAWOW, MORSKIE_OKO, DATE_XML, DATE_SYSTEM) VALUES (#{xml_parse(xml_windDIR.to_s)}, '#{xml_date.to_s}', '#{datetime}');")
+db.query("INSERT INTO temperature (GORYCZKOWA, PIEC_STAWOW, MORSKIE_OKO, DATE_XML, DATE_SYSTEM) VALUES (#{xml_parse(xml_temperature.to_s)}, '#{xml_date}', '#{datetime}');")
+db.query("INSERT INTO wind_speed_averange (GORYCZKOWA, PIEC_STAWOW, MORSKIE_OKO, DATE_XML, DATE_SYSTEM) VALUES (#{xml_parse(xml_windAVG.to_s)}, '#{xml_date}', '#{datetime}');")
+db.query("INSERT INTO wind_speed_maximum (GORYCZKOWA, PIEC_STAWOW, MORSKIE_OKO, DATE_XML, DATE_SYSTEM) VALUES (#{xml_parse(xml_windMAX.to_s)}, '#{xml_date}', '#{datetime}');")
+db.query("INSERT INTO wind_direction (GORYCZKOWA, PIEC_STAWOW, MORSKIE_OKO, DATE_XML, DATE_SYSTEM) VALUES (#{xml_parse(xml_windDIR.to_s)}, '#{xml_date}', '#{datetime}');")
