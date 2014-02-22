c43f0850b6529168919a7c835cee325f8646c2df
Your Name <you@example.com>
2014-02-22 21:28:09 +0000

    new options

--- grab_avalanche.rb
+++ grab_avalanche.rb
@@ -13,13 +13,25 @@ av_tend = url.css('div.tendency img').map { |link| link['alt'] }
 av_tend = av_tend.to_s.split(" ")
 av_tend = av_tend[1].to_s.gsub('"',"").gsub("]","").gsub(".","")
 
-if "#{av_tend}" == "rosn±ca" 
-  puts "#{av_level.chars[2].to_i}+"
-elsif "#{av_tend}" == "malej±ca"
-  puts "#{av_level.chars[2].to_i}-"
-elsif "#{av_tend}" == "stala"
-  puts "#{av_level.chars[2].to_i}"
-else
-  puts "NULL"
+begin
+  db = Mysql.new('localhost', 'ruby', 'github', 'topr')
+rescue Mysql::Error
+  puts "Oh noes! Damn... we could not connect to our database. -.-;"
+  exit 1
 end
 
+xml = Nokogiri::XML(open("http://www.test.tatrynet.pl/pogoda/weatherMiddleware_v1.0/xml/lokalizacje1.xml"),'utf-8')
+xml_date = xml.xpath('//dateTimeStr').map { |node| node.text }.first
+
+if "#{av_tend}" == "rosn±ca" 
+  db.query("INSERT INTO avalanche_level (LEVEL, TEND, DATE_XML, DATE_SYSTEM) VALUES (#{av_level.chars[2].to_i}, '+', '#{xml_date.to_s}', '#{Time.now.utc.to_s.gsub(" UTC","")}');")
+  #puts "#{av_level.chars[2].to_i}+"
+elsif "#{av_tend}" == "malej±ca"
+  db.query("INSERT INTO avalanche_level (LEVEL, TEND, DATE_XML, DATE_SYSTEM) VALUES (#{av_level.chars[2].to_i}, '-', '#{xml_date.to_s}', '#{Time.now.utc.to_s.gsub(" UTC","")}');")  
+  #puts "#{av_level.chars[2].to_i}-"
+elsif "#{av_tend}" == "stala"
+  db.query("INSERT INTO avalanche_level (LEVEL, TEND, DATE_XML, DATE_SYSTEM) VALUES (#{av_level.chars[2].to_i}, ' ', '#{xml_date.to_s}', '#{Time.now.utc.to_s.gsub(" UTC","")}');")
+  #puts "#{av_level.chars[2].to_i}"
+else
+  db.query("INSERT INTO avalanche_level (LEVEL, TEND, DATE_XML, DATE_SYSTEM) VALUES (#{av_level.chars[2].to_i}, NULL, '#{xml_date.to_s}', '#{Time.now.utc.to_s.gsub(" UTC","")}');")
+end
