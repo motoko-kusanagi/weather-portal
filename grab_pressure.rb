918b7e05bea1c99a620d909bb6c8d84556127d21
Your Name <you@example.com>
2014-02-23 07:43:55 +0000

    fixed location

--- grab_pressure.rb
+++ grab_pressure.rb
@@ -20,4 +20,4 @@ end
 
 datetime = Time.now.utc.to_s.gsub(" UTC","")
 
-db.query("INSERT INTO pressure (KASPROWY_WIERCH, LOMNICA, DATE_SYSTEM) VALUES (#{text_parse(pressure[25].text)}, #{text_parse(pressure[30].text)}, '#{datetime}');")
+db.query("INSERT INTO pressure (KASPROWY_WIERCH, LOMNICA, DATE_SYSTEM) VALUES (#{text_parse(pressure[20].text)}, #{text_parse(pressure[30].text)}, '#{datetime}');")
